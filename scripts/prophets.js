51b4a17f3afd94011623b08f74834f95e2a46e78
FerAmadorSolano <fernanda.ama25@gmail.com>
2026-09-14 21:46:37 -0600

    learning activities

---
+++ scripts/prophets.js
@@ -0,0 +1,54 @@
+// Current year
+const year = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+// Last modified date
+document.getElementById("lastModified").innerHTML =
+    `Last Modified: ${document.lastModified}`;
+
+// Prophets
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let birthDate = document.createElement('p');
+        let birthPlace = document.createElement('p');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute(
+            'alt',
+            `Portrait of ${prophet.name} ${prophet.lastname}`
+        );
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+
+
+        cards.appendChild(card);
+    });
+};
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    displayProphets(data.prophets);
+}
+
+getProphetData();
