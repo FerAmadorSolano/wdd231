015e1f36fd71808047d5792d7c0b59216f56e88d
FerAmadorSolano <fernanda.ama25@gmail.com>
2026-09-17 22:53:22 -0600

    chamber of commerce directory

---
+++ chamber_project/scripts/directory.js
@@ -0,0 +1,86 @@
+// Current year
+const year = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+// Last modified date
+document.getElementById("lastModified").innerHTML =
+    `Last Modified: ${document.lastModified}`;
+
+// Menu Button
+const menuBtn = document.querySelector("#menu-btn");
+const navigation = document.querySelector("nav");
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+
+    if (navigation.classList.contains("open")) {
+        menuBtn.textContent = "✕";
+    } else {
+        menuBtn.textContent = "☰";
+    }
+});
+
+// Members
+const membersContainer = document.querySelector("#members");
+
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+
+const url = "data/members.json";
+
+async function getMembers() {
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    displayMembers(data.members);
+}
+
+function displayMembers(members) {
+
+    membersContainer.innerHTML = "";
+
+    members.forEach(member => {
+
+        const card = document.createElement("article");
+
+        card.classList.add("member-card");
+
+        card.innerHTML = `
+        <img src="images/${member.image}" 
+            alt="${member.name} logo"
+            loading="lazy">
+
+        <div class="member-info">
+            <h3>${member.name}</h3>
+
+            <p>${member.address}</p>
+
+            <p>${member.phone}</p>
+
+            <a href="${member.website}" target="_blank" rel="noopener">
+                Visit Website
+            </a>
+        </div>
+    `;
+
+        membersContainer.appendChild(card);
+    });
+}
+
+gridButton.addEventListener("click", () => {
+
+    membersContainer.classList.add("member-grid");
+    membersContainer.classList.remove("member-list");
+
+});
+
+listButton.addEventListener("click", () => {
+
+    membersContainer.classList.add("member-list");
+    membersContainer.classList.remove("member-grid");
+
+});
+
+getMembers();
