7cb95f77ed49a8ba3624c956e264e5891c689b22
FerAmadorSolano <fernanda.ama25@gmail.com>
2026-09-22 22:03:10 -0600

    beginning of the home page

---
+++ week3/scripts/api.js
@@ -0,0 +1,24 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat={49.75}&lon={6.64}&units=imperial&appid={7497ae954a7ae3d027416056f26763de}';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
